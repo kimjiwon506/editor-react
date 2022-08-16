cdb25eb0c7f670e0215caf7ee4feab410484e43b
kimjiwon506 <kimjiwon506@gmail.com>
2022-08-16 16:16:14 +0900

    feat:axios기본 url설정 및 axios로 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080"
 
 function App() {
   return (
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import Box from '../components/box';
 import Editor from '../components/Editor';
 
 const MainPage = () => {
+
+  useEffect(()=>{
+    (async ()=>{
+      const {data} = await axios.get("/tmp")
+      console.log(data)
+    })()
+  },[])
   return (
     <Box p="16px">
       <h1>클라우드메모장</h1>
